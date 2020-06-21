1579389531b7a693dbd1bb64b12d85a8034932d3
engcfraposo <engcfraposo@gmail.com>
2020-06-20 22:52:12 -0300

    Pagina de Appoinments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hi World'})
-})
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
-})
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333!');
+});
